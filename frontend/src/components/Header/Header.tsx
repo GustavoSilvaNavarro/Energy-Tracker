84039fa54aa83119a286306b7b654a8efa11661c
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-12-20 14:53:51 -0300

    feat(frontend-build): frontend React App was build for production

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -31,7 +31,7 @@ export const Header = () => {
             </label>
             <DatePicker
               id="dateByYear"
-              disabled={productionCtx?.loadingStatus}
+              disabled={productionCtx && productionCtx.loadingStatus ? true : false}
               className="bg-gray-50 border border-gray-300 text-center text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               selected={currentDate}
               minDate={new Date('01/01/2000')}
