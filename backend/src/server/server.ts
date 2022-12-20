ae8abc5387c2d52e65b6a01f71b7200f59b31ee9
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-12-20 19:46:22 -0300

    build(setup): set up for production backend

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 
 import env from '../config/env';
 import stateDetailsRoutes from '../routes/stateDetails-routes';
@@ -9,7 +9,7 @@ import { errorHandler } from '../middleware/error-handler';
 
 const app: Application = express();
 
-app.set('port', process.env.PORT || env.dbAppPort);
+app.set('port', process.env.PORT || env.dbAppPort || 8080);
 
 app.use(express.json());
 app.use(
@@ -17,7 +17,7 @@ app.use(
     origin: env.clientAppUrl,
   })
 );
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 app.use('/states-list', stateDetailsRoutes);
 
